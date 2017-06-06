06127b7b7f2a05ff9fe0496cc1452665ae157b91
Tiago Tiede <tiago@navut.com>
2017-06-06 15:44:10 -0300

    Refactor app

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,7 +9,6 @@ class ImagesController < ApplicationController
     image_service = ImagesWebService.new(CONFIG['images_web_service_url'])
 
     Image.create_from_urls(image_service.photos_urls, request.base_url)
-
-    head :ok, content_type: 'text/html'
+    render json: 'The system is running a assync process, it may take a while to generate all the photos.', status: 200
   end
 end
