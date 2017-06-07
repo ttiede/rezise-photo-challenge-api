a2277e1ad1667bbfe3cb7eb5e3899c2d1c328c96
Tiago Tiede <tiago@navut.com>
2017-06-07 16:25:54 -0300

    Controller Test

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ImagesController, type: :controller do
+	describe "GET populate" do
+		it "has a 200 status code" do
+			get :populate
+			expect(response.status).to eq(200)
+		end
+	end
+	describe "GET show" do
+		it "has a 200 status code" do
+			get :show
+			expect(response.status).to eq(200)
+		end
+	end	
 end
